2c34861e4212d6e1a89fd09bcac34e8cfff91663
sergio128 <sergio128@users.noreply.github.com>
2021-09-09 18:30:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 // Sets server port and logs message on success
-app.listen(80 || 80, () => console.log('webhook is listening'));
+app.listen(process.env.PORT || 80, () => console.log('webhook is listening'));
 
 // Creates the endpoint for our webhook 
 app.post('/webhook', (req, res) => {  
